0937416489b31f4488483d9ef9da8534eaf1aee2
DakitaNorth <garryant3120@yandex.ru>
2023-03-07 11:46:37 +0300

    Add project files.

---
+++ App1/App1/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Entities
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public string Unit { get; set; }
+
+
+    }
+}
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using App1.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public UserAuth CurrentUser { get; set; }
+        public MainPage()
+        {
+            CurrentUser = new UserAuth();
+
+            InitializeComponent();
+        }
+
+        private async void Sign_In(object sender, EventArgs e)
+        {
+            /*var client = new WebClient();
+            client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+            var result = client.UploadString("API user reg", JsonConvert.SerializeObject(CurrentUser));*/
+
+ 
+
+            await Navigation.PushAsync(new Main());
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
